783c19ca6fabbf8f6a3d05b1baebf00da538a312
milliVilla <soledad110203@gmail.com>
2024-04-10 16:39:28 -0300

    punto4 terminado

--- punto2.py
+++ punto2.py
@@ -3,11 +3,16 @@
 # "digitos" es la lista de numero en la que uno deve faltar 
 def encontrar_numero_faltantes(n, digitos):
     try:
+        # Genera un conjunto de números del 1 al n
         lista_de_iteracion = set(range(1, n + 1)) # genera un conjunto de numeros que va del 1 al n, con set convertimos los num en conjunto
+        # Convierte la lista de dígitos en un conjunto
         numeros_ejemplos= set(digitos)
+        # Encuentra los números faltantes restando los conjuntos
         numero_faltantes= lista_de_iteracion - numeros_ejemplos # utilizo "-" para encontra los elem que estan en la lista_de_iteracion pero no estan en los numeros_ejemplo , donde esto nos dara los numeros faltantes.
+        # Extrae y devuelve el número faltante utilizando pop
         return  numero_faltantes.pop() #extrae el elemento faltante  con el pop y lo devuelve como resultado
     except:
+        # Si ocurre algún error durante el proceso, devuelve un mensaje de error
         return "error al realizar la busqueda del numero faltante"
     
 print(encontrar_numero_faltantes(5,[1,2,4,5]))
--- punto4.py
+++ punto4.py
@@ -1,42 +1,33 @@
-#
-# def comprobar_palindromo(palabra):
-#     palabra= palabra.lower() #lower para convertir la palabra que ingreses en minuscula
-#     palabra_invertido= palabra == palabra[::-1] #retorna la palabra con la vercion invertidad de derecha a izquierda
-#     if palabra==palabra_invertido:
-#      return palabra
+def convertir_palindrome(palabra):
     
-# print(comprobar_palindromo("oso"))
-
-def comprobar_palindromo(caracteres):
-    convertir_minuscula= caracteres.lower()
-    lista_palabras= convertir_minuscula.split()
+    caracter_unicos = set(palabra) # ordena en un conjunto los caracteres unicos , osea elimina las letras que se repiten
+    orden_letras = sorted(caracter_unicos, reverse=True) # ordena las letras de forma descendente (de mayor a menor) y devuelve una nueva lista con estos elementos ordenados
     
-    lista_sin_espacios=[]
+    palidromo_lista = [] # se crea una lista vacia para los valores del palindromo
+    letra_impar = [] 
+    # se crea un for que recorre cada letras en la lista "orden_letra"
+    for letra in orden_letras:
+        count = palabra.count(letra)  # y cuenta cuantas veces aparece esa letra en la cadena "palabra"
+       
+    # se verifica si el recuento de las letras es par o impar
+        if count % 2 == 0: #verifica si el contador es par 
+            #si es par entonces 
+            for _ in range(count//2):
+                # se inserta la letra  al principio y al final en la lista
+                palidromo_lista.insert(0,letra)
+                palidromo_lista.append(letra)
+    
+        else:
+            # si es impar la letra se retorna esto
+            if letra_impar:
+                return 'NO SOLUTION' 
+            
+            palidromo_lista.insert(len(palidromo_lista) // 2,letra)
+            letra_impar = True
 
-    for palabra in lista_palabras:
-        limpiar= palabra.strip(".").strip(",")
-        lista_sin_espacios.append(limpiar)
-        unir_cadena= "".join(lista_sin_espacios)
-    # Generar todas las subcadenas
-    subcadenas = []
-    for i in range(len(unir_cadena)):
-        for j in range(i + 1, len(unir_cadena) + 1):
-            subcadenas.append(unir_cadena[i:j])
+    return ''.join(palidromo_lista) # une todas las letras sin separarlas
 
-    # Comprobar si las subcadenas son palíndromos
-    palindromos = []
-    for subcadena in subcadenas:
-        es_palindromo = True
-        for i in range(len(subcadena) // 2):
-            if subcadena[i] != subcadena[len(subcadena) - 1 - i]:
-                es_palindromo = False
-                break
-        if es_palindromo:
-            palindromos.append(subcadena)
+# caso de prueba
+assert convertir_palindrome("aabbc") == "abcba", "Error en el caso de prueba"
 
-    # Imprimir los palíndromos encontrados
-    for palindromo in palindromos:
-        print(palindromo)
-
-
-print(comprobar_palindromo("hola como estas ana yo tengo un oso"))
+print('todos los casos de pruba han pasado correctamente.')  
